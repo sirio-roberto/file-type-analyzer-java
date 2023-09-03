751d121f6d657adf4923ca88e1ff87640a65ea92
Sirio <sirioroberto1@hotmail.com>
2023-09-03 17:43:14 -0300

    Implemented KMP algorithm

--- analyzer/Main.java
+++ analyzer/Main.java
@@ -3,13 +3,14 @@ package analyzer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         if (args.length == 4) {
             String algorithm = args[0];
             String filePath = args[1];
-            String patternStr = args[2];
+            String patternStr = getPattenFromArg(args[2]);
             String resultingType = args[3];
 
             try {
@@ -28,6 +29,10 @@ public class Main {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    private static String getPattenFromArg(String arg) {
+        return arg.substring(arg.lastIndexOf('%'));
     }
 
     private static boolean isKMP(String algorithm) {
@@ -53,11 +58,45 @@ public class Main {
     }
 
     private static boolean findUsingKMP(String pattern, String text) {
-        return true;
+        int[] prefixFunc = getPrefixFunction(pattern);
+
+        for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
+            if (text.charAt(i) == pattern.charAt(0)) {
+                boolean isMatch = true;
+                int k = i;
+                for (int j = 0; j < pattern.length(); j++) {
+                    if (pattern.charAt(j) != text.charAt(k)) {
+                        isMatch = false;
+                        i += j - 1 - prefixFunc[j - 1];
+                        break;
+                    }
+                    k++;
+                }
+                if (isMatch) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static int[] getPrefixFunction(String text) {
+        int[] function = new int[text.length()];
+        for (int i = 0; i < function.length; i++) {
+            int maxPrefix = 0;
+            String substring = text.substring(0, i + 1);
+            for (int j = 0; j < substring.length(); j++) {
+                if (substring.substring(0, j).equals(substring.substring(substring.length() - j))) {
+                    maxPrefix = j;
+                }
+            }
+            function[i] = maxPrefix;
+        }
+        return function;
     }
 
     private static boolean findUsingNaive(String pattern, String text) {
-        for (int i = 0; i < text.length() - pattern.length(); i++) {
+        for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
             if (text.charAt(i) == pattern.charAt(0)) {
                 boolean isMatch = true;
                 for (char c : pattern.toCharArray()) {
